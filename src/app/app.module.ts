d5def1e839513080dd095c1f7e8acfb6b909bbaa
AshanSenadheeraLK <github@ashansenadheera.lk>
2025-05-15 17:56:46 +0530

    Fix reChapta 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module } from 'ng-recaptcha';
+import { RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module } from 'ng-recaptcha-19';
 
 @NgModule({
   imports: [
-    BrowserModule,
     CommonModule,
-    AppComponent,
-    RouterModule.forRoot([]),
     RecaptchaV3Module
   ],
   providers: [
     { provide: RECAPTCHA_V3_SITE_KEY, useValue: "6Le9bDsrAAAAAM8A_mgmxg4aILmM9nmQBDelD2rz" }
+  ],
+  exports: [
+    RecaptchaV3Module
   ]
 })
 export class AppModule { } 
