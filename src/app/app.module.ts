6c7ef377b9741834b98e6e91b294bcb309e4905e
AshanSenadheeraLK <github@ashansenadheera.lk>
2025-05-15 17:47:29 +0530

    ReChapta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module } from 'ng-recaptcha';
 
 @NgModule({
   imports: [
-    AppComponent,
+    BrowserModule,
     CommonModule,
-    RouterModule.forChild([
-      { path: '', component: AppComponent }
-    ])
+    AppComponent,
+    RouterModule.forRoot([]),
+    RecaptchaV3Module
+  ],
+  providers: [
+    { provide: RECAPTCHA_V3_SITE_KEY, useValue: "6Le9bDsrAAAAAM8A_mgmxg4aILmM9nmQBDelD2rz" }
   ]
 })
 export class AppModule { } 
